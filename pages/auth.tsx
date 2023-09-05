9e122eea6a76f693b3c2ed020ac4e66b9f9efcdb
abhishek <abhiy145@gmail.com>
2023-09-05 22:19:42 +0530

    login/ register functionallity

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,20 +1,65 @@
+"use client";
 import { Input } from "@/components";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { useCallback, useState } from "react";
+import { getSession, signIn } from "next-auth/react";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const Auth = () => {
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const [variant, setVariant] = useState('login');
-  
-    const toggleVariant = useCallback(() => {
-      setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
-    }, []);
-  
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    }).then((callback) => {
+      if (callback?.ok) {
+        toast.success("Logged in");
+        router.push("/profile");
+      }
+      if (callback?.error) {
+        if (callback.error === "Email does not exist") {
+          toast.error("Email does not exist");
+        } else {
+          toast.error(callback.error);
+        }
+      }
+    });
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+      toast.success("registered successfully");
+    } catch (error: any) {
+      if (error.response.status === 422) {
+        toast.error("Email is already taken");
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
+  }, [email, name, password]);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <Toaster />
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <Image
@@ -28,23 +73,23 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-            {variant === 'login' ? 'Sign in' : 'Register'}
+              {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
-            {variant === 'register' && (
+              {variant === "register" && (
                 <Input
                   id="name"
                   type="text"
                   label="Username"
                   value={name}
-                  onChange={(e: any) => setName(e.target.value)} 
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
 
               <Input
                 id="email"
                 type="email"
-                label="Email address or phone number"
+                label="Email Id"
                 value={email}
                 onChange={(e: any) => setEmail(e.target.value)}
               />
@@ -56,13 +101,21 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-            {variant === 'login' ? 'Login' : 'Sign up'}
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
-              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
               .
             </p>
